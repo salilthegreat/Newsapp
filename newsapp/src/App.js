b6967af946f5510c6f2cb7a778415d32da1db21d
salilthegreat <salilkumardas22@gmail.com>
2023-04-04 00:53:20 -0700

    Can access differnet category of news through  Navbar

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 export default class App extends Component {
   c = 'John';
@@ -11,11 +11,18 @@ export default class App extends Component {
     return (
       <div>
         <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element= {<News pageSize ={6} country={"us"} category = {"science"}/>}/>
-        </Routes>
-        
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<News key="Home" pageSize={6} country={"us"} category={"general"} />} />
+            <Route exact path="/general" element={<News key="general" pageSize={6} country={"us"} category={"general"} />} />
+            <Route exact path="/science" element={<News key="science" pageSize={6} country={"us"} category={"science"} />} />
+            <Route exact path="/business" element={<News key="business" pageSize={6} country={"us"} category={"business"} />} />
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={6} country={"us"} category={"entertainment"} />} />
+            <Route exact path="/health" element={<News key="health" pageSize={6} country={"us"} category={"health"} />} />
+            <Route exact path="/sports" element={<News key="sports" pageSize={6} country={"us"} category={"sports"} />} />
+            <Route exact path="/technology" element={<News key="technology" pageSize={6} country={"us"} category={"technology"} />} />
+          </Routes>
+
         </Router>
       </div>
     )
